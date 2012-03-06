cb71914a6f9e555b5e663d9b639e9e8c56f3cd64
Robert Wallhead <rwallhead@gmail.com>
2012-03-06 18:10:02 +1100

    Fixes from horrible logic typos and algorithm changes too
    Had XOR testing around wrong way somehow, also changes to order of
    test data training. Not perfect by a long shot but the tests pass...

--- trainer.py
+++ trainer.py
@@ -3,9 +3,8 @@ import collections
 
 class Trainer:
 
-    def __init__(self, nn, outputreversals, memorysize):
+    def __init__(self, nn, memorysize):
         self.nn = nn
-        self.outputreversals = outputreversals
         self.memory = collections.deque([], memorysize) #input=>target store
 
     @staticmethod
@@ -15,23 +14,22 @@ class Trainer:
         return 0
 
     @staticmethod
-    def _iterations(maxiters, quesize, index):
-        return maxiters
+    def _reverse(value):
+        return 1 - value
 
-    def getoutputs(self, inputs):
+    def getoutput(self, inputs):
         return tuple(map(Trainer._normalise, self.nn.update(inputs)))
 
     def bad(self, inputs, outputs):
-        outputs = tuple(map(Trainer._normalise, outputs))
-        targets = self.outputreversals[outputs]
+        targets = tuple(map(Trainer._reverse, outputs))
         self.memory.append((inputs, targets))
 
     def good(self, inputs, outputs):
-        targets = tuple(map(Trainer._normalise, outputs))
+        targets = tuple(outputs)
         self.memory.append((inputs, targets))
 
     def train(self, iterations=1000):
-        for i in range(len(self.memory)):
-            for j in range(Trainer._iterations(iterations, self.memory.maxlen, i)):
+        for j in range(iterations):
+            for i in range(len(self.memory)):
                 self.nn.update(self.memory[i][0])
                 self.nn.backPropagate(self.memory[i][1], 0.5, 0.1)
