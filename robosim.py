b886a6805c90ebd91883e7ff1277cb68230cdae4
Robert Wallhead <rwallhead@gmail.com>
2012-03-11 16:56:28 +1100

    Sim now static, and can be printed to show state.
    Probably will change this, but was worth it to learn __metaclass__

--- robosim.py
+++ robosim.py
@@ -1,36 +1,109 @@
 import copy
 
 
-class Terrain:
+class Terrain(object):
 
     FLOOR = 0
     WALL = 1
     ROBOT = 8
 
 
-class Robot:
+class Robot(object):
+    """ A robot.
+    """
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angle = 0
+        self.hit = [False, False]
+
+    def turnleft45(self):
+        self.angle = Sim.fixangle(self.angle - 45)
+        self._updatesensors()
+
+    def turnright45(self):
+        self.angle = Sim.fixangle(self.angle + 45)
+        self._updatesensors()
+
+    def turnleft90(self):
+        self.angle = Sim.fixangle(self.angle - 90)
+        self._updatesensors()
+
+    def turnright90(self):
+        self.angle = Sim.fixangle(self.angle + 90)
+        self._updatesensors()
+
+    def turn180(self):
+        self.angle = Sim.fixangle(self.angle + 180)
+        self._updatesensors()
+
+    def _updatesensors(self):
+        self.hit = [False, False]
+        if not Sim.passable(Sim.location(self.x, self.y, Sim.fixangle(self.angle - 45))):
+            self.hit[0] = True
+        if not Sim.passable(Sim.location(self.x, self.y, Sim.fixangle(self.angle + 45))):
+            self.hit[1] = True
+        if ((self.hit == [False, False] and
+             self.angle % 45 == 0 and not
+             Sim.passable(Sim.location(self.x, self.y, self.angle)))
+            or
+            (self.angle % 45 != 0 and not
+             Sim.passable(Sim.location(self.x, self.y, self.angle)))):
+             self.hit = [True, True]
+
+    def forward(self):
+        x, y = Sim.location(self.x, self.y, self.angle)
+        if Sim.passable((x, y)):
+            self.x = x
+            self.y = y
+        self._updatesensors()
+
+
+class SimMeta(type):
+    def __str__(self):
+        mapcopy = copy.deepcopy(Sim.mapdata)
+        mapcopy[Sim.robot.y][Sim.robot.x] = "R"
+        visual = ""
+        visual += "/".ljust(Sim.mapwidth + 1, "-") + "\\\n"
+        for row in range(Sim.mapheight):
+            for col in range(Sim.mapwidth):
+                if col == 0:
+                    visual += "|"
+                visual += str(mapcopy[row][col])
+                if col == Sim.mapwidth - 1:
+                    visual += "|"
+            visual += "\n"
+        visual += "\\".ljust(Sim.mapwidth + 1, "-") + "/"
+        return visual
+
+
+class Sim(object):
     """ A very basic robot simulator. Robot moves forward or backward, in one
         of 8 directions (chosen by rotating on spot).
     """
 
-    def __init__(self, size, mapdata, x, y):
+    __metaclass__ = SimMeta
+
+    @staticmethod
+    def setup(mapdata, robot):
         """ Setup, load the mapfile.
         """
-        self.mapdata = mapdata
-        self.mapwidth = len(self.mapdata)
-        self.mapheight = len(self.mapdata[0])
-        self.angle = 0
-        self.x = x
-        self.y = y
-        self.hit = [False, False]
+        Sim.mapdata = mapdata
+        Sim.mapwidth = len(mapdata[0])
+        Sim.mapheight = len(mapdata)
+        Sim.robot = robot
 
-    @property
-    def state(self):
-        mapcopy = copy.deepcopy(self.mapdata)
-        mapcopy[self.y][self.x] = Terrain.ROBOT
-        return mapcopy
+    @staticmethod
+    def passable(xy):
+        x = xy[0]
+        y = xy[1]
+        return (x >= 0 and y >= 0 and
+                x < Sim.mapwidth and y < Sim.mapheight and
+                Sim.mapdata[y][x] == Terrain.FLOOR)
 
-    def _fixangle(self, angle):
+    @staticmethod
+    def fixangle(angle):
         if angle < 0:
             angle += 360
         if angle > 360:
@@ -39,48 +112,8 @@ class Robot:
             angle = 0
         return angle
 
-    def _passable(self, xy):
-        x = xy[0]
-        y = xy[1]
-        return (x >= 0 and y >= 0 and
-                x < self.mapwidth and y < self.mapheight and
-                self.mapdata[y][x] in (Terrain.ROBOT, Terrain.FLOOR))
-
-    def turnleft45(self):
-        self.angle = self._fixangle(self.angle - 45)
-        self._updatesensors()
-
-    def turnright45(self):
-        self.angle = self._fixangle(self.angle + 45)
-        self._updatesensors()
-
-    def turnleft90(self):
-        self.angle = self._fixangle(self.angle - 90)
-        self._updatesensors()
-
-    def turnright90(self):
-        self.angle = self._fixangle(self.angle + 90)
-        self._updatesensors()
-
-    def turn180(self):
-        self.angle = self._fixangle(self.angle + 180)
-        self._updatesensors()
-
-    def _updatesensors(self):
-        self.hit = [False, False]
-        if not self._passable(self._location(self.x, self.y, self._fixangle(self.angle - 45))):
-            self.hit[0] = True
-        if not self._passable(self._location(self.x, self.y, self._fixangle(self.angle + 45))):
-            self.hit[1] = True
-        if ((self.hit == [False, False] and
-             self.angle % 45 == 0 and not
-             self._passable(self._location(self.x, self.y, self.angle)))
-            or
-            (self.angle % 45 != 0 and not
-             self._passable(self._location(self.x, self.y, self.angle)))):
-             self.hit = [True, True]
-
-    def _location(self, x, y, direction):
+    @staticmethod
+    def location(x, y, direction):
         if direction == 0:
             y-=1
         if direction == 45:
@@ -102,10 +135,3 @@ class Robot:
             x-=1
             y-=1
         return x, y
-
-    def forward(self):
-        x, y = self._location(self.x, self.y, self.angle)
-        if self._passable((x, y)):
-            self.x = x
-            self.y = y
-        self._updatesensors()
